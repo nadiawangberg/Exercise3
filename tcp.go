25755998e1322cefe0d6d568daadd461bc004485
Your Name <you@example.com>
2020-01-29 19:49:09 +0100

    task3 done :)

--- tcp.go
+++ tcp.go
@@ -15,13 +15,14 @@ func checkError(err error) {
 	
 }
 
-func read() {
+func read(listener *TCPConn) {
 	buffer := make([]byte, 1024)
-	tcp_addr, err := ResolveTCPAddr("tcp", "10.100.23.147:33546")
-	listener, err := DialTCP("tcp", nil, tcp_addr) // connects to a server
-	checkError(err)
+	//tcp_addr, err := ResolveTCPAddr("tcp", "10.100.23.147:33546")
+	//listener, err := DialTCP("tcp", nil, tcp_addr) // connects to a server
+	//checkError(err)
 	Println("Hei!")
 	for {
+		Println("Hei2!")
 		_,err := listener.Read(buffer)
 		checkError(err)
 		Printf(string(buffer))
@@ -34,14 +35,16 @@ func main() {
 	checkError(err)
 	conn, err := DialTCP("tcp", nil, tcp_addr)
 	checkError(err)
+	conn.Write([]byte("Connect to: 10.100.23.147:33546\x00"))
 	
-	read()
+	go read(conn)
 	
 	for {
+
 		time.Sleep(100*time.Millisecond)
 		// WRITE
-		Println("Er du der server??")
-		_, err := conn.Write([]byte("Eg er her\n\x00")) // \x00
+		//Println("Er du der server??")
+		_, err := conn.Write([]byte("Jeg er her\n\x00")) // \x00
 		checkError(err)		
 	}
 }
